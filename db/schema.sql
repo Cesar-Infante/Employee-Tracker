dd551dcd3383ae1d6a7fce5d157b719b07483ee7
Cesar's PC <cesar261110@gmaill.com>
2022-08-09 15:34:09 -0700

    Updates to schema for department, roles, employee

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,26 @@ CREATE DATABASE employerTracker_db;
 
 USE employerTracker_db;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT,
+    FOREIGN KEY (roles_id),
+    REFERENCES roles(id),
+    manager_id INT NOT NULL
+);
