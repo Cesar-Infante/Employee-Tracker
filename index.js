34d0c2a7027be5e9a6e0d68277a1763b043a3578
cesar-infante <cesar261110@gmail.com>
2022-08-10 19:36:34 -0700

    init function complete to start application and be prompted with options

--- index.js
+++ index.js
@@ -27,9 +27,73 @@ const initApp = [
     },
 ];
 
+const appOptions = [
+    {
+        type: "input",
+        name: "addDept",
+        message: "What is the name of the current department you are adding?",
+    },
+    {
+        type: "input",
+        name: "addRole",
+        message: "What is the name of the role you are adding?",
+    },
+    {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary of the role?",
+    },
+    {
+        type: "input",
+        name: "roleDept",
+        message: "Which department does the role belong to?",
+    },
+    {
+        type: "input",
+        name: "firstName",
+        message: "What is the current employee's first name?",
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the current employee's last name?",
+    },
+    {
+        type: "input",
+        name: "employeeRole",
+        message: "What is the current employee's role?",
+    },
+    {
+        type: "input",
+        name: "employeeManager",
+        message: "Who is the current employee's manager?",
+    },
+];
+
+
+
 
 async function init() {
-    let responses = await inquirer.prompt(initApp)  
+    await inquirer.prompt(initApp).then((data, err) => {
+        if (data.initApp === "View All Departments") {
+            getDept()
+        } else if (data.initApp === "View All Roles") {
+            getRoles()
+        } else if (data.initApp === "View All Employees") {
+            getEmployees()
+        } else if (data.initApp === "Add A Department") {
+            // addDept()
+        } else if (data.initapp === "Add A Role") {
+            // addRole()
+        } else if (data.initApp === "Add A Employee") {
+            // addEmployee()
+        } else if (data.initApp === "Update A Employee Role") {
+            // updateRole
+        } else {
+            console.log(err);
+        }
+    })
 }
 
 init()
+
